8b12eb8e61c2841291e54824434441b2e694de35
xuqian <xuqian@uhope.com>
2018-07-26 10:40:29 +0800

    添加springbooteurekaconfigserver  配置管理server

---
+++ SpringBootEurekaConfigTest/src/test/java/com/uhope/test/SpringBootEurekaConfigTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uhope.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootEurekaConfigTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
