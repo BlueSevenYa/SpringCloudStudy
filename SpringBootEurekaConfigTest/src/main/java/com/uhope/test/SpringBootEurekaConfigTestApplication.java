56788e30ad72bd6ba15b3a47331ef8f99cf0add3
xuqian <xuqian@uhope.com>
2018-07-26 13:47:03 +0800

    提交

--- SpringBootEurekaConfigTest/src/main/java/com/uhope/test/SpringBootEurekaConfigTestApplication.java
+++ SpringBootEurekaConfigTest/src/main/java/com/uhope/test/SpringBootEurekaConfigTestApplication.java
@@ -3,10 +3,10 @@ package com.uhope.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer
 @EnableConfigServer
 public class SpringBootEurekaConfigTestApplication {
 
